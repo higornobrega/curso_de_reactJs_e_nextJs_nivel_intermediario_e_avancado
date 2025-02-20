2465c8aec3f26562f00ced260f323250cbe1d37e
Higor Nóbrega <higorst.nobrega@gmail.com>
2025-02-20 17:29:45 -0300

    Adicionar estrutura inicial do projeto-1 com arquivos de configuração, componentes e testes

---
+++ curso-reactjs-nextjs-project-1-68d1976480a3e310aed0647efc2a132f5ddd4738/project-1/src/App.js
@@ -0,0 +1,55 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Component } from "react";
+
+class App extends Component {
+  state = {
+    cont: 0,
+    posts: [
+      {
+        id: 1,
+        title: "O título 1",
+        body: "O corpo 1",
+      },
+      {
+        id: 2,
+        title: "O título 2",
+        body: "O corpo 2",
+      },
+      {
+        id: 3,
+        title: "O título 3",
+        body: "O corpo 3",
+      },
+   ],
+  };
+  // Acontece após os componentes serem montados/carregados
+  componentDidMount() {
+   this.handleTimeout()
+  }
+
+  handleTimeout = () => {
+    const { cont, posts } = this.state
+    posts[0].title = 'O Título Mudou'
+    setTimeout(() => {
+      this.setState({ posts, cont: cont + 1})
+    }, 5000)
+  }
+
+  render() {
+    const { cont, posts } = this.state;
+
+    return (
+      <div className="App">
+        <h1>{cont}</h1>
+        {posts.map((post) => (
+          <div key={post.id}>
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+export default App;
